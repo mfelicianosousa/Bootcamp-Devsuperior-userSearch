d07f439263c87ae6f27cd9977b90ae71c2d9ac9e
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-02-06 22:12:10 -0400

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './core/assets/styles/custom.scss' ;
+import Navbar from './core/components/Navbar' ;
+import './app.scss';
 
 const App = () => {
 
-    return <h1>  Hello App !!! </h1>
-};
+    return (
+        <Navbar />
+
+    ) ;
+}
 
 export default App ;
---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => {
+
+    return (
+        <nav className="row bg-primary main-nav">
+           <div className="col-3">
+             <a href="Link" className="nav-logo-text">
+                    <h4> Bootcamp DevSuperior</h4>
+             </a>
+               
+           </div>
+         
+
+        </nav>
+
+    ) ;
+}
+
+export default Navbar ;
